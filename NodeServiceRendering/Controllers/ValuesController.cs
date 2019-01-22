1f8b095d4cc83ba460aa9620c4d98a3593c1ad61
Chan Wen <chawe@microsoft.com>
2019-01-22 16:53:07 +0800

    changes

---
+++ NodeServiceRendering/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.NodeServices;
+
+namespace NodeServiceRenderingSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly INodeServices nodeServices;
+
+        public ValuesController(INodeServices nodeServices)
+        {
+            this.nodeServices = nodeServices;
+        }
+       
+        [HttpGet("render")]
+        public async Task<ActionResult<string>> RenderByNode()
+        {
+            var htmlContent = await nodeServices.InvokeAsync<string>("./wwwroot/js/renderMyNpm");
+            return Content(htmlContent, "text/html");
+        }
+
+      
+    }
+}
